bdb33896300de68481825f902533b720e004ac58
DanieleBernassola <danielebernassola02@gmail.com>
2024-04-29 17:22:03 +0200

    [UPD] Added inputs and button

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 'use strict';
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
-const kilometers = Number(prompt('Inserisci i chilometri che vuoi percorrere'));
-console.log(kilometers);
-const age = Number(prompt('Inserisci la tua età'));
-console.log(age);
+const buttonReadText = document.querySelector('.generate-form');
+console.log(buttonReadText);
+
+buttonReadText.addEventListener('click', function(){
+    const elementInputKm = document.querySelector('[type="text"]');
+    console.log(elementInputKm.value);
+    const elementInputAge = document.querySelector('[type="number"]');
+    console.log(elementInputAge.value);
+})
+
+const generaForm = document.getElementById('generaForm');
+console.log(generaForm);
+
+generaForm.addEventListener('submit', function (event){
+    event.preventDefault();
+})
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-const prezzoPieno = kilometers * 0.21;
+const prezzoPieno = elementInputKm * 0.21;
 let prezzoScontato = prezzoPieno;
 
-if(!isNaN(kilometers) && !isNaN(age)) {
+if(!isNaN(elementInputKm) && !isNaN(elementInputAge)) {
     console.log('chilometri e età sono dei numeri');
     console.log('Questo è il prezzo iniziale:', prezzoPieno);
-    if(age < 18) {
+    if(elementInputAge < 18) {
         // va applicato uno sconto del 20% per i minorenni
         prezzoScontato = prezzoPieno - (prezzoPieno * 20 / 100);
-    } else if(age >= 65){
+    } else if(elementInputAge >= 65){
         // va applicato uno sconto del 40% per gli over 65.
         prezzoScontato = prezzoPieno - (prezzoPieno * 40 / 100);
     }
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
- - definisco una variabile kilometers e salvo il valore di un prompt
- - definisco una variabile age e salvo il valore di un prompt
+ - definisco una variabile kilometers e salvo il valore di un input
+ - definisco una variabile age e salvo il valore di un input
+ - aggiungo un bottone per inviare i dati
 
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -15,7 +16,6 @@ va applicato uno sconto del 20% per i minorenni
  - trasformo age in numero da una stringa
  - definisco una variabile prezzoScontato
  
-
 va applicato uno sconto del 40% per gli over 65.
 
  - SE age < 18
