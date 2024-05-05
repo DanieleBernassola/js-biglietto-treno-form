913cf4a68357faf8bf872198a5d3d3cc7c8fe804
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-05 15:49:03 +0200

    [UPD] Added ticket result in html

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
 // Bottone che crea il ticket
 const buttonGenerate = document.querySelector('.generate-form');
+const buttonReset = document.querySelector('.reset-form');
 
 // Avviene quando si clicca sul bottone genera
 buttonGenerate.addEventListener('click', function () {
@@ -11,8 +12,16 @@ buttonGenerate.addEventListener('click', function () {
     const ticketResult = document.getElementById('generated-ticket');
 
     // Dati di input
+    const userNameInput = document.getElementById('name-user');
     const kmInput = document.getElementById('km-input');
     const ageInput = document.getElementById('age-input');
+
+    // Variabili per risultato del biglietto
+    const userNameOutput = document.getElementById('name-output');
+    const offerOutput = document.getElementById('offer-output');
+    const carOutput = document.getElementById('car-output');
+    const codeOutput = document.getElementById('code-output');
+    const priceOutput = document.getElementById('price-output');
 
     // Dati per il prezzo del biglietto
     const priceKm = 0.21;
@@ -34,5 +43,18 @@ buttonGenerate.addEventListener('click', function () {
     console.log(kmInput.value);
     console.log(price.toFixed(2));
 
+    // Risultato biglietto
+    userNameOutput.innerText = userNameInput.value;    
+    offerOutput.innerText = ageInput.value;
+    carOutput.innerText = Math.ceil(Math.random() * 20);
+    codeOutput.innerText = Math.ceil(Math.random() * 89999 + 10000);
+    priceOutput.innerText = `${price.toFixed(2)}€`;    
+
     ticketResult.classList.remove('hidden');
 });
+
+buttonReset.addEventListener('click', function() {
+    const ticketResult = document.getElementById('generated-ticket');
+
+    ticketResult.classList.add('hidden');
+})
